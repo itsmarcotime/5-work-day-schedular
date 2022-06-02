695edd89894b5c889c46f839694fc996ff68881f
Marco Mata <patrickstar1281@yahoo.com>
2022-06-02 06:41:17 -0500

    finished with localstorage! can now save & reload page!

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -32,7 +32,7 @@ $(".description").each(function() {
 });
 
 
-//HELP HERE
+//save to localstorage!
 var saveStuff = function(e) {
 
     e.preventDefault();
@@ -48,15 +48,15 @@ var saveStuff = function(e) {
 
 function loadStuff() {
 
-    $("#9 .description").val(localStorage.getItem("9"));
-    $("#10 .description").val(localStorage.getItem("10"));
-    $("#11 .description").val(localStorage.getItem("11"));
-    $("#12 .description").val(localStorage.getItem("12"));
-    $("#13 .description").val(localStorage.getItem("13"));
-    $("#14 .description").val(localStorage.getItem("14"));
-    $("#15 .description").val(localStorage.getItem("15"));
-    $("#16 .description").val(localStorage.getItem("16"));
-    $("#17 .description").val(localStorage.getItem("17"));
+    $("#9").val(localStorage.getItem("9"));
+    $("#10").val(localStorage.getItem("10"));
+    $("#11").val(localStorage.getItem("11"));
+    $("#12").val(localStorage.getItem("12"));
+    $("#13").val(localStorage.getItem("13"));
+    $("#14").val(localStorage.getItem("14"));
+    $("#15").val(localStorage.getItem("15"));
+    $("#16").val(localStorage.getItem("16"));
+    $("#17").val(localStorage.getItem("17"));
 
 };
 
